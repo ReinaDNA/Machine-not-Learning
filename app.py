1b2088a28a49c988a95cfe5399286d969e85781d
ReinaDNA <xxzhijiexx@gmail.com>
2026-03-08 22:59:58 +0800

    removed stuff

--- app.py
+++ app.py
@@ -51,15 +51,5 @@ def home():
         dew_point=dew_point
     )
 
-# @app.route("/forecast")
-# def forecast():
-#     city="Kuala Lumpur"
-
-#     url=f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric"
-
-#     response = requests.get(url)
-#     data = response.json()
-
-
 if __name__ == "__main__":
     app.run(debug=True)
